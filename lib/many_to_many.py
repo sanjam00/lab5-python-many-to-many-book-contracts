47aeb87f97c30df091f7384beb6450e96b96ec25
Sanaya James <sanaya.j.james@gmail.com>
2026-02-12 23:41:51 -0700

    properties of Contract

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -18,3 +18,43 @@ class Contract:
 		self.book = book
 		self.date = date
 		self.royalties = royalties
+
+	@property
+	def author(self):
+		return self._author
+	
+	@author.setter
+	def author(self, value):
+		if not isinstance (value, Author):
+			raise Exception
+		self._author = value
+
+	@property
+	def book(self):
+		return self._book
+	
+	@book.setter
+	def book(self, value):
+		if not isinstance(value, Book):
+			raise Exception
+		self._book = value
+
+	@property
+	def date(self):
+		return self._date
+	
+	@date.setter
+	def date(self, value):
+		if not isinstance (value, str):
+			raise Exception
+		self._date = value
+
+	@property
+	def royalties(self):
+		return self._royalties
+	
+	@royalties.setter
+	def royalties(self, value):
+		if not isinstance(value, int):
+			raise Exception
+		self._royalties = value
